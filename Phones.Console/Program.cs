403cb8d6e76a0a9e0d341e44efb8dbe77b06dc5e
Ilya Reva <ilya.reva@gmail.com>
2016-05-18 22:52:07 +0300

    Add hashtable resolve
    1.72 c

---
+++ Phones.Console/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OlimClimbing.Phone
+{
+    public static class Program
+    {
+        public class Node
+        {
+            public Node(char value = default(char))
+            {
+                Digit = value;
+            }
+
+            public char Digit { get; private set; }
+            public bool IsDialed { get; set; }
+            public Hashtable Child { get; set; }
+
+
+        }
+
+        public class DigitalTree
+        {
+
+
+            private readonly Node root;
+
+            public DigitalTree()
+            {
+                this.root = new Node();
+            }
+
+            public bool Add(string number)
+            {
+                var currentNode = root;
+                foreach (var digit in number)
+                {
+                    //Create node for adding
+                    var nodeToAdd = new Node(digit);
+
+                    //No nodes
+                    if (currentNode.Child == null)
+                    {
+                        currentNode.Child = new Hashtable { { digit, nodeToAdd } };
+                        currentNode = nodeToAdd;
+                        continue;
+                    }
+
+                    //Find node
+                    var finded = (Node)currentNode.Child[digit];
+                    if (finded != null)
+                    {
+                        if (finded.IsDialed)
+                        {
+                            return false;
+                        }
+                        currentNode = finded;
+                    }
+                    else
+                    {
+                        currentNode.Child.Add(digit, nodeToAdd);
+                        currentNode = nodeToAdd;
+                    }
+                }
+                if (currentNode.Child != null)
+                {
+                    return false;
+                }
+                currentNode.IsDialed = true;
+                return true;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+
+
+            List<string> lines = new List<string>();
+            string l;
+            while ((l = System.Console.ReadLine()) != null)
+            {
+
+                lines.Add(l);
+            }
+
+
+            var testCases = new List<string[]>();
+            var testCasesCount = int.Parse(lines[0]);
+            int totalStrreaded = 0;
+
+
+            for (int i = 0; i < testCasesCount; i++)
+            {
+                var currentHeadOfTestCase = i + totalStrreaded + 1;
+                var phonesCount = int.Parse(lines[currentHeadOfTestCase]);
+                totalStrreaded += phonesCount;
+                var testcase = new string[phonesCount];
+                for (int j = 0; j < phonesCount; j++)
+                {
+                    testcase[j] = lines[currentHeadOfTestCase + 1 + j];
+                }
+                testCases.Add(testcase);
+            }
+
+            foreach (var testCase in testCases)
+            {
+                
+                Console.WriteLine(Is(testCase) ? "YES" : "NO");
+            }
+
+            
+        }
+
+        public static bool Is(string[] phones)
+        {
+            var tree = new DigitalTree();
+            return phones.All(p => tree.Add(p));
+        }
+    }
+}
---
+++ Phones.Tests/PhoneTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using OlimClimbing.Phone;
+
+namespace Phones.Tests
+{
+    [TestFixture]
+    public class PhoneTests
+    {
+        [SetUp]
+        public void Init()
+        {
+
+        }
+
+        [Test(Description = "Test cases for digital tree")]
+        [TestCaseSource(typeof(DigitalCasesFactory),nameof(DigitalCasesFactory.TestCases))]
+        public bool AssertDigitalTree(string[] phones)
+        {
+            var tree = new Program.DigitalTree();
+            return phones.All(phone => tree.Add(phone));
+        }
+    }
+
+    public static class DigitalCasesFactory
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData((object)new string[] { "911", "97625999", "91125426" }).Returns(false);
+                yield return new TestCaseData((object)new string[] {"1", "23", "4", "4", "234"}).Returns(false);
+                yield return new TestCaseData((object)new string[] {"123", "124", "12"}).Returns(false);
+                yield return new TestCaseData((object)new string[] {"123", "124", "13"}).Returns(true);
+            }
+        }
+    }
+}
